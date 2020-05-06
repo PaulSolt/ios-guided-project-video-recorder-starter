d5f27a8b967b16b264eb39b21945ff12d057ddca
Paul Solt <PaulSolt@gmail.com>
2020-05-06 12:49:48 -0400

    Added video + microphone permission.

--- VideoRecorder/VideoRecorder/ViewController.swift
+++ VideoRecorder/VideoRecorder/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 	
@@ -15,14 +16,40 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view.
 	}
 	
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
-		
-		// TODO: get permission
-		
-		showCamera()
-		
-	}
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Get permission
+        requestPermissionAndShowCamera()
+    }
+
+    private func requestPermissionAndShowCamera() {
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+        
+        switch status {
+            case .notDetermined:  // first time we've requested access
+                requestPermission()
+            case .restricted: // parental controls prevent user from using the camera / microphone
+                fatalError("Tell user they need to request permission from parent (UI)")
+            case .denied:
+                fatalError("Tell user to enable in Settings: Popup from Audio to do this, or use a custom view")
+            case .authorized:
+                showCamera()
+            default:
+                fatalError("Handle new case for authorization")
+        }
+    }
+    
+    private func requestPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { (granted) in
+            guard granted else {
+                fatalError("Tell user to enable in Settings: Popup from Audio to do this, or use a custom view")
+            }
+            DispatchQueue.main.async {
+                self.showCamera()
+            }
+        }
+    }
 	
 	private func showCamera() {
 		performSegue(withIdentifier: "ShowCamera", sender: self)
