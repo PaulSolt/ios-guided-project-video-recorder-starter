3a361bf7f44c4fa3778b9b615721a5e4a983a738
Paul Solt <PaulSolt@gmail.com>
2020-06-03 12:59:42 -0400

    Added captureSession Stubs

--- VideoRecorder/VideoRecorder/CameraViewController.swift
+++ VideoRecorder/VideoRecorder/CameraViewController.swift
@@ -7,20 +7,57 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
 
+    lazy var captureSession = AVCaptureSession()
+    
     @IBOutlet var recordButton: UIButton!
     @IBOutlet var cameraView: CameraPreviewView!
-
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		// Resize camera preview to fill the entire screen
 		cameraView.videoPlayerView.videoGravity = .resizeAspectFill
+        
+        setUpCaptureSession()
 	}
 
+    private func setUpCaptureSession() {
+        
+        captureSession.beginConfiguration()
+        // Inputs
+        
+        // Camera
+        
+        // Microphone
+        
+        // Quality level
+        
+        // Outputs
+        
+        // Set the captureSession into our CameraPreviewView
+        
+        captureSession.commitConfiguration()
+        cameraView.session = captureSession
+    }
+    
+    private func bestCamera() -> AVCaptureDevice {
+        // ideal camera, fallback camera, FUTURE: we add a button to choose front/back
+        if let ultraWideCamera = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+            return ultraWideCamera
+        }
+        
+        if let wideAngleCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) { // try .front
+            return wideAngleCamera
+        }
+        
+        // Simulator or the requested hardware cameras doesn't work!
+        fatalError("No camera available, are you on a simulator?") // TODO: show UI instead of a fatal error
+    }
+    
 
     @IBAction func recordButtonPressed(_ sender: Any) {
 
