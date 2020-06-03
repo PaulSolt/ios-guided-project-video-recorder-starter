231ef31f99a9004b90f3616320f0d8eae2f5ce95
Paul Solt <PaulSolt@gmail.com>
2020-06-03 12:43:57 -0400

    Requested permission for video and microphone.

--- VideoRecorder/VideoRecorder/ViewController.swift
+++ VideoRecorder/VideoRecorder/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 	
@@ -15,15 +16,46 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view.
 	}
 	
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
-		
-		// TODO: get permission
-		
-		showCamera()
-		
-	}
-	
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+                
+        // view must be on screen before we can transition to a new screen
+        requestPermissionAndShowCamera()
+    }
+
+    private func requestPermissionAndShowCamera() {
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+        
+        switch status {
+            case .authorized:
+                // 2nd time user has used app (they've already authorized)
+                showCamera()
+            case .denied:
+                // 2nd time user has used app (they have not given permission)
+                // take to the settings app (or show a custom Onboarding screen to explain why need access)
+                fatalError("Show user UI to get them to give access")
+            case .notDetermined:
+                // 1st time user is using app
+                requestPermission()
+            case .restricted:
+                // Parental controls (need to inform user they don't have access, maybe ask parents?)
+                fatalError("Show user UI to request permssion from boss/parent/self")
+        }
+    }
+
+    private func requestPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { (granted) in
+            guard granted else {
+                fatalError("Show user UI to get them to give access")
+                // return  // TODO show UI for getting privacy permission
+            }
+            
+            DispatchQueue.main.async {
+                self.showCamera()
+            }
+        }
+    }
+    
 	private func showCamera() {
 		performSegue(withIdentifier: "ShowCamera", sender: self)
 	}
